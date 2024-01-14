b23f7107ae3e0183985c0957d2310ec1778d1604
Vivek Kumar Patel <vivekece1116@gmail.com>
2024-01-14 10:17:26 +0530

    zomato navbar is done

---
+++ zomato/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CreateOrderComponent } from './pages/create-order/create-order.component';
+import { RestaurantItemsComponent } from './pages/restaurant-items/restaurant-items.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CreateOrderComponent,
+    RestaurantItemsComponent,
+    CategoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
